2b768b8c9121a4ad4f2e0317e145c821140c20f3
tangzhenyu69-ux <tangzhenyu69@gmail.com>
2026-09-10 11:40:48 +0800

    do a test

---
+++ src/helloworld/test.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入想要输出的语句");
+        String a = sc.next();
+        System.out.println(a);
+
+    }
+
+}
